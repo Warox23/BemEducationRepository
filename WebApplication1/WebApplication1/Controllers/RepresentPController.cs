91aa142eeed4bdb9e5cf0e6fb665c93581765c76
poraiko <44779221+poraiko@users.noreply.github.com>
2018-11-06 16:05:40 +0200

    new

---
+++ WebApplication1/WebApplication1/Controllers/RepresentPController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class RepresentPController : Controller
+    {
+        [Route("represent")]
+        public IActionResult RepresentP()
+        {
+            SomeModel x = new SomeModel();
+            x.Text = "AnotherName";
+            
+            return View(x);
+        }
+    }
+}
